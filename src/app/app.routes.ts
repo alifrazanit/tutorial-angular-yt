1dc7dc8fa436ccae827b673c08b35d3210e18367
Alif Razan Saputra <alifrazanit@gmail.com>
2023-12-17 20:52:04 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './template/admin/admin.component';
+import { PortofolioWebComponent } from './template/portofolio-web/portofolio-web.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { 
+        path: '',
+        component: PortofolioWebComponent
+    },
+    { 
+        path: 'admin',
+        component: AdminComponent,
+    }
+];
